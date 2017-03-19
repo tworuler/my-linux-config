a572a1996a7c97afeaf29551505bc329d448e483
tworuler <tworuler.ycl@gmail.com>
2017-03-19 18:14:48 +0000

    add tmux powerline install

--- tmux/install.sh
+++ tmux/install.sh
@@ -4,3 +4,16 @@ echo "Update tmux config..."
 cp tmux.conf ~/.tmux.conf
 tmux source ~/.tmux.conf
 echo "Done!"
+
+if [[ -d ~/.tmux/tmux-powerline ]]
+then
+    echo "~/.tmux/tmux-powerline exist"
+else
+    echo "Install tmux powerline"
+    git clone https://github.com/erikw/tmux-powerline.git ~/.tmux/tmux-powerline
+    echo "Done!"
+fi
+echo "Update tmux powerline config..."
+cp my-tmux-powerline-themes.sh ~/.tmux/tmux-powerline/themes/default.sh
+echo "Done!"
+
--- tmux/my-tmux-powerline-themes.sh
+++ tmux/my-tmux-powerline-themes.sh
@@ -28,8 +28,8 @@ if [ -z $TMUX_POWERLINE_LEFT_STATUS_SEGMENTS ]; then
 		#"ifstat 30 255" \
 		#"ifstat_sys 30 255" \
 		"lan_ip 24 255 ${TMUX_POWERLINE_SEPARATOR_RIGHT_THIN}" \
-		#"wan_ip 24 255" \
-		#"vcs_branch 29 88" \
+		"wan_ip 24 255" \
+		"vcs_branch 29 88" \
 		"vcs_compare 60 255" \
 		"vcs_staged 64 255" \
 		"vcs_modified 9 255" \
@@ -45,8 +45,8 @@ if [ -z $TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS ]; then
 		#"mailcount 9 255" \
 		#"now_playing 234 37" \
 		#"cpu 240 136" \
-		#"load 237 167" \
-		"tmux_mem_cpu_load 234 136" \
+		"load 237 167" \
+		#"tmux_mem_cpu_load 234 136" \
 		"battery 137 127" \
 		#"weather 37 255" \
 		#"rainbarf 0 ${TMUX_POWERLINE_DEFAULT_FOREGROUND_COLOR}" \
