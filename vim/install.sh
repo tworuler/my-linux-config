ec6580350504d86b5baf8654b90dc80ef9498cee
tworuler <tworuler.ycl@gmail.com>
2017-03-19 15:07:45 +0000

    update vim install

--- vim/install.sh
+++ vim/install.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-echo "Install Vundle..."
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim 
-echo "Done!"
+if [[ -d ~/.vim/bundle/Vundle.vim ]]
+then
+    echo "Vundle has already been installed."
+else
+    echo "Install Vundle..."
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim 
+    echo "Done!"
+fi
 
 echo "Update vimrc..."
 cp vimrc ~/.vimrc
