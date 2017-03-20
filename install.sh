a215d52a593f0f25bf55e2b9051be6705dc4ab39
tworuler <tworuler.ycl@gmail.com>
2017-03-20 11:48:59 +0800

    add install all config and powerline fonts

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+config_list=(
+    vim
+    tmux
+    zsh
+)
+
+for dir in ${config_list[@]}
+do
+    cd $dir
+    ./install.sh
+    cd ..
+done
+
---
+++ tmux/install-powerline-fonts.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+git clone https://github.com/powerline/fonts.git
+cd fonts
+./install.sh
+cd ..
+
