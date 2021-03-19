4c2fe53f98b4797be6b6d591f725a51bb811f817
Chiliang Yang <tworuler.ycl@gmail.com>
2021-03-19 10:55:22 +0800

    fix tmux conf

--- tmux/install.sh
+++ tmux/install.sh
@@ -6,13 +6,10 @@ pushd "$(dirname "$0")"
 
 echo "$tmux_version"
 echo "Update tmux config..."
-if [[ $tmux_version == 2.1 ]]; then
-    cp tmux.conf ~/.tmux.conf
-elif [[ $tmux_version == 2.* ]]; then
-    # tmux utf8 option removed after 2.2
-    grep -v 'status-utf8' tmux.conf > ~/.tmux.conf
-else
+if [[ $tmux_version == 1.* ]]; then
     sed 's/ -c "#{pane_current_path}"//' tmux.conf > ~/.tmux.conf
+else
+    cp tmux.conf ~/.tmux.conf
 fi
 tmux source ~/.tmux.conf
 echo "Done!"
