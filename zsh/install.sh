ab0b07216ae986440d378d55a2d971c9de0daaa8
Chiliang Yang <tworuler.ycl@gmail.com>
2022-09-18 11:18:28 +0000

    update zsh install

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,50 +1,24 @@
 #!/bin/bash
 
-pushd "$(dirname "$0")"
+set -ex
 
-if [[ -d ~/.oh-my-zsh ]]; then
-    echo "~/.oh-my-zsh exists."
-else
-    echo "Install oh-my-zsh..."
+DIR=$(cd "$(dirname "$0")"; pwd)
+
+if [[ ! -d ~/.oh-my-zsh ]]; then
     git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
-    echo "Done!"
 fi
 
-echo "update oh-my-zsh themes..."
-ls | grep "\.zsh-theme" | xargs -I {} bash -c 'echo "copy {}" && cp {} ~/.oh-my-zsh/themes/'
-echo "Done!"
+ls ${DIR}/*.zsh-theme | xargs -I {} cp {} ~/.oh-my-zsh/themes/
+cp ${DIR}/my.zshrc ~/.oh-my-zsh/my.zshrc
 
-echo "update .zshrc"
-cp my.zshrc ~/.oh-my-zsh/my.zshrc
-add_source=$(cat << EOM
-# Load my common config
-[[ -s ~/.oh-my-zsh/my.zshrc ]] && source ~/.oh-my-zsh/my.zshrc
-EOM
-)
+LOAD_ZSH="[[ -s ~/.oh-my-zsh/my.zshrc ]] && source ~/.oh-my-zsh/my.zshrc"
 if [[ ! -e ~/.zshrc ]]; then
-    echo -e "$add_source\n" >  ~/.zshrc
-elif [[ "$(grep 'source ~/.oh-my-zsh/my.zshrc' ~/.zshrc)" == "" ]]; then
-    echo -e "$add_source\n" | cat - ~/.zshrc > tmp && mv tmp ~/.zshrc
+    echo -e "${LOAD_ZSH}\n" >  ~/.zshrc
+elif [[ $(grep "${LOAD_ZSH}" ~/.zshrc) == "" ]]; then
+    echo -e "${LOAD_ZSH}\n" | cat - ~/.zshrc > ${DIR}/tmp && mv ${DIR}/tmp ~/.zshrc
 fi
-echo "Done!"
-
-# if [[ -d ~/.autojump ]]; then
-#     echo "~/.autojump exists."
-# else
-#     echo "Install autojump..."
-#     git clone https://github.com/joelthelion/autojump.git
-#     cd autojump
-#     ./install.py
-#     cd ..
-#     echo "Done!"
-# fi
 
 if [[ ! -e ~/.oh-my-zsh/z.sh ]]; then
-    echo "Install z.sh..."
-    git clone https://github.com/rupa/z.git
-    cp z/z.sh ~/.oh-my-zsh/z.sh
-    echo "Done!"
+    curl -fLo ~/.oh-my-zsh/z.sh https://raw.githubusercontent.com/rupa/z/master/z.sh
 fi
-
-popd
 
