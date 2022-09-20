e7e5e32228b45d012bc0da556e83636f4a02de8c
Chiliang Yang <tworuler.ycl@gmail.com>
2022-09-20 21:54:41 +0800

    add build android

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 docker pull ubuntu:22.04
-docker build . -t base-dev:ubuntu22.04 --target base-dev
+docker build . -t base-dev:ubt22.04 --target base-dev
+docker build . -t android-dev:ubt22.04 --target android-dev
